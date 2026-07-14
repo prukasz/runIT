38177aab022ac96ea001f52046ab8464edb3dfa4
Prukasz <krolpaziolandii@gmail.com>
2026-07-14 19:30:06 +0200

    leftover file edits to fit new code

--- _SOFTWARE/main/main.c
+++ _SOFTWARE/main/main.c
@@ -1,18 +1,12 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "rik_main.h"
+#include "runit.h"
 
 #define TAG __FILE_NAME__
 
 void app_main(void) {
-    ESP_LOGI(TAG, "=== RIK ===");
-
-    rik_start();
-
-    ESP_LOGI(TAG, "RIK Started");
-
-    while (1) {
-        vTaskDelay(portMAX_DELAY);
-    }
-
+  runit_start();
+  while (1) {
+    vTaskDelay(portMAX_DELAY);
+  }
 }
